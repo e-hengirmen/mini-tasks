96caf20691c261485d3c0eea4c67fe0664f4cc1f
Ersel Hengirmen <ehengirmen@hotmail.com>
2020-07-19 18:46:44 +0300

    Added Binary Search
    Closes: #2
    
    Now we have more then just sorting algorithms

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # mini-tasks
-## [merge_sort.cpp](https://github.com/e-hengirmen/mini-tasks/blob/master/merge_sort.cpp)
+## [merge_sort.cpp](https://github.com/e-hengirmen/mini-tasks/blob/master/sort/merge_sort.cpp)
 Contains merge sort implementation written in C++
-## [quick_sort.cpp](https://github.com/e-hengirmen/mini-tasks/blob/master/quick_sort.cpp)
+## [quick_sort.cpp](https://github.com/e-hengirmen/mini-tasks/blob/master/sort/quick_sort.cpp)
 Contains quick sort implementation written in C++
+## [binary_search.cpp](https://github.com/e-hengirmen/mini-tasks/blob/master/search/binary_search.cpp)
+Contains binary search implementation written in C++
---
+++ search/binary_search.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+int Binary_Search(int *arr,int n,int k){
+    int x=arr[n/2];
+    if(n==1){
+        if(*arr==k)return 1;
+        else return -1;                     //-1 means that this array does not have searched number
+    }
+    if(k==x){
+        return n/2+1;
+    }
+    else if(k<x){                           
+        int y=Binary_Search(arr,n/2,k);
+        if(y==-1)return -1;
+        return y;
+        
+    }
+    else{
+        int y=Binary_Search(arr+n/2+1,n-n/2-1,k);
+        if(y==-1)return -1;
+        return n/2+1+y;
+
+    }
+
+}
+
+
+
+int main(){
+    int n,k,result,sum=0;
+    std::cin>>n>>k;                             //entering array size and number to be searched in the right order
+    int a[n];
+    for(int *i=a;i<a+n;i++)std::cin>>*i;     //entering array
+    std::sort(a,a+n);                        //sorting using STL libraries because this implementation is about searching
+    result=Binary_Search(a,n,k);
+    if(!result)std::cout<<"The array does not contain "<<k<<std::endl;
+    else std::cout<<k<<" is in position "<<result<<std::endl;
+}
